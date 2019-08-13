3ca7c2b051a267dd2b426929baae807c26f9bf96
ennshi <enn.shi@gmail.com>
2019-08-13 21:14:16 +0200

    NPM scripts p.1

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('#mycarousel').carousel({interval: 2000});
+    $('#carouselButton').click(function() {
+        if($('#carouselButton').children('span').hasClass('fa-pause')){
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        } else {
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+    });
+    $('#loginBtn').click(function() {
+        $('#loginModal').modal('toggle');
+    });
+    $('#reserveBtn').click(function() {
+        $('#reserveModal').modal('toggle');
+    });
+    $('#loginCancelBtn').click(function() {
+        $('#loginModal').modal('hide');
+    });
+     $('#reserveCancelBtn').click(function() {
+        $('#reserveModal').modal('hide');
+    });
+});
